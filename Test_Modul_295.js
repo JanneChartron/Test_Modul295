39266db0c9dad373350b8f9ff703db4d7b34c8ed
Janne <janne.chartron@bbw.ch>
2023-12-20 14:26:41 +0100

    Teil2

--- Test_Modul_295.js
+++ Test_Modul_295.js
@@ -1,12 +1,3 @@
-/**
- *
- * Autor: Janne
- * Date: 20.12.2023
- * This code can use Authenticator to access a self-made library that contains
- * tasks that you have to do.
- *
- */
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -20,35 +11,90 @@ const logindata = {
 
 let isAuthenticated = false;
 
-const generateRandomToken = () => {
-  return crypto.randomBytes(10).toString("hex");
-};
-
 app.use(bodyParser.json());
 
 // Parser für JSON-Daten
 app.use(express.json());
 
-let tasks = [
-  { id: 1, description: "Task 1", status: "todo" },
-  { id: 2, description: "Task 2", status: "in-progress" },
-];
+// Array zum Speichern gültiger Tokens
+let validTokens = [];
 
-const checklogindata = (req, res, next) => {
-    if (logindata) {
-      next();
-    } else {
-      res.status(403).json({ message: 'You are Unauthorized' });
-    }
-  };
+// Funktion zur Generierung eines zufälligen Tokens
+const generateRandomToken = () => {
+  return crypto.randomBytes(10).toString('hex'); // Beispiel: 10 Bytes entsprechen 20 hexadezimalen Zeichen
+};
+
+// Middleware zur Überprüfung des Tokens
+const checkTokenValidity = (req, res, next) => {
+  const token = req.headers.authorization; // Hole den Token aus dem Authorization-Header
+
+  if (isValidToken(token)) {
+    next(); // Wenn der Token gültig ist, setze die Anfrage fort
+  } else {
+    res.status(401).json({ message: 'Invalid token' }); // Wenn der Token ungültig ist, sende einen Fehlerstatus
+  }
+};
+
+// Funktion zur Überprüfung der Token-Gültigkeit
+const isValidToken = (token) => {
+  return validTokens.includes(token);
+};
+
+// Funktion zum Hinzufügen eines gültigen Tokens
+const addValidToken = (token) => {
+  validTokens.push(token);
+};
+
+// Funktion zum Entfernen eines gültigen Tokens
+const removeValidToken = (token) => {
+  const index = validTokens.indexOf(token);
+  if (index !== -1) {
+    validTokens.splice(index, 1);
+  }
+};
+
+// POST /login
+app.post('/logins', (req, res) => {
+  const { benutzername, password } = req.body;
+
+  if (benutzername === logindata.benutzername && password === logindata.password) {
+    const token = generateRandomToken();
+    addValidToken(token); // Füge den generierten Token zu den gültigen Tokens hinzu
+    res.status(200).json({ message: 'Your token is:', token });
+  } else {
+    res.status(401).json({ message: 'Your login details are incorrect' });
+  }
+});
+
+// GET /verify
+app.get('/verifys', checkTokenValidity, (req, res) => {
+  res.status(200).json({ message: 'Your Token is valid' });
+});
+
+// DELETE /logout
+app.delete('/logouts', checkTokenValidity, (req, res) => {
+  const token = req.headers.authorization; // Hole den Token aus dem Authorization-Header
+  removeValidToken(token); // Entferne den Token aus den gültigen Tokens
+  res.status(204).send();
+});
+
+// Check if the Cookie is valid
+app.get('/valides', checkTokenValidity, (req, res) => {
+  res.status(200).json({ message: 'Authenticated request' });
+});
+
+const tasks = [
+    { id: 1, description: "Task 1", status: "todo" },
+    { id: 2, description: "Task 2", status: "in-progress" },
+  ];
 
 // GET /tasks
-app.get("/tasks", (req, res) => {
+app.get("/tasks", checkTokenValidity, (req, res) => {
   res.status(200).json(tasks);
 });
 
 // POST /tasks
-app.post("/tasks", (req, res) => {
+app.post("/tasks", checkTokenValidity, (req, res) => {
   const newTask = req.body;
 
   newTask.id = tasks.length + 1;
@@ -57,7 +103,7 @@ app.post("/tasks", (req, res) => {
 });
 
 // GET /tasks/{id}
-app.get("/tasks/:id", (req, res) => {
+app.get("/tasks/:id", checkTokenValidity, (req, res) => {
   const taskId = parseInt(req.params.id);
   const foundTask = tasks.find((task) => task.id === taskId);
 
@@ -69,7 +115,7 @@ app.get("/tasks/:id", (req, res) => {
 });
 
 // PUT /tasks/{id}
-app.put("/tasks/:id", (req, res) => {
+app.put("/tasks/:id", checkTokenValidity, (req, res) => {
   const taskId = parseInt(req.params.id);
   const updatedTask = req.body;
 
@@ -84,7 +130,7 @@ app.put("/tasks/:id", (req, res) => {
 });
 
 // DELETE /tasks/{id}
-app.delete("/tasks/:id", (req, res) => {
+app.delete("/tasks/:id", checkTokenValidity, (req, res) => {
   const taskId = parseInt(req.params.id);
   const deletedTaskIndex = tasks.findIndex((task) => task.id === taskId);
 
@@ -95,59 +141,6 @@ app.delete("/tasks/:id", (req, res) => {
     res.status(404).json({ error: "Task not found" });
   }
 });
-
-// Funktion zur Überprüfung des Tokens
-const checkTokenValidity = (req, res, next) => {
-    const token = req.headers.authorization; // Du solltest den Token aus dem Authorization-Header oder einem anderen sicheren Ort erhalten
-  
-    // Überprüfe, ob der Token gültig ist (ersetze dies durch deine eigene Logik)
-    if (isValidToken(token)) {
-      next(); // Token ist gültig, fahre fort
-    } else {
-      res.status(401).json({ message: 'Invalid token' }); // Token ist ungültig
-    }
-  };
-  
-  // Beispiel-Funktion zur Überprüfung der Token-Gültigkeit (ersetze dies durch deine eigene Logik)
-  const isValidToken = (token) => {
-    // Hier solltest du deine eigene Logik zur Überprüfung des Tokens implementieren
-    // Zum Beispiel, vergleiche den Token mit den in deiner Anwendung gespeicherten gültigen Tokens
-    // Rückgabe true, wenn der Token gültig ist, ansonsten false
-    return validTokens.includes(token);
-  };
-  
-  // Annahme: validTokens ist ein Array, das gültige Tokens speichert
-  const validTokens = [];
-  
-  // POST /login
-  app.post('/logins', (req, res) => {
-    const { benutzername, password } = req.body;
-  
-    if (benutzername === logindata.benutzername && password === logindata.password) {
-      const token = generateRandomToken();
-      validTokens.push(token); // Füge den generierten Token zu den gültigen Tokens hinzu
-      res.status(200).json({ message: "Your token is:", token });
-    } else {
-      res.status(401).json({ message: "Your login details are incorrect" });
-    }
-  });
-  
-  // GET /verify
-  app.get('/verifys', checkTokenValidity, (req, res) => {
-    res.status(200).json({ message: 'Your Token is valid' });
-  });
-  
-  // DELETE /logout
-  app.delete('/logouts', (req, res) => {
-    const token = req.headers.authorization; // Hole den Token aus dem Authorization-Header
-    validTokens = validTokens.filter((t) => t !== token); // Entferne den Token aus den gültigen Tokens
-    res.status(204).send();
-  });
-  
-  // Check if the Cookie is valid
-  app.get('/valides', checkTokenValidity, (req, res) => {
-    res.status(200).json({ message: 'Authenticated request' });
-  });
 
 // Output Port
 app.listen(port, () => {
